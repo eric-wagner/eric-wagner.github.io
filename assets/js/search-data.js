efbd3a57c3cc42062d495afe6ade067922f619d0
eric-wagner <eric-w-@hotmail.com>
2025-12-15 22:58:42 +0000

    Deploying to gh-pages from @ eric-wagner/eric-wagner.github.io@10472db6d0bf94ef5fdb02d47e0efeb641fd0534 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-about-me",
+          title: "About Me",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/";
+          },
+        },{id: "nav-publications",
           title: "Publications",
           description: "",
           section: "Navigation",
