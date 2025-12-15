3e228a3ce8199aca5c675b0795ad764d2004aa2d
eric-wagner <eric-w-@hotmail.com>
2025-12-15 02:24:21 +0000

    Deploying to gh-pages from @ eric-wagner/eric-wagner.github.io@16520d3800d13fb99e956275099253448d3e26df 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,7 +42,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/wagner2025caiba.pdf";
+            window.location.href = "/assets/pdf/cv_eric_wagner.pdf";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
