36d38df3f4a6751570ae70c88af2d98e92ede345
Eric Wagner <eric-w-@hotmail.com>
2026-05-10 01:35:09 +0200

    Daily cron update

--- daily_push.sh
+++ daily_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/leudelange/website/eric-wagner.github.io
+source ~/.venv/website/bin/activate
 python ./bin/update_scholar_citations.py
 git add .
 git commit -m "Daily cron update"
